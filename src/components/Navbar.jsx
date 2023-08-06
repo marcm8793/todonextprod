dd68cb2065ccd0811f5f8024b2a03a1ce748b21a
Marcosss87 <marcmansour@outlook.fr>
2023-08-06 22:00:43 +0200

    this is the one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <div className="container flex justify-between items-center p-5">
       <div className="flex items-center">
-        <Image src="/note.svg" alt="Logo" width={50} height={50}></Image>
+        <Image src="/note.svg" alt="Logo" width={50} height={50}></Image> 
         <p className=" font-extrabold text-blue-800 text-3xl p-2">NextApps</p>
       </div>
       <div className="space-x-5 flex items-center">
