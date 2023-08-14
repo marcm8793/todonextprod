6ed2880c690fdf7cdd286c7651947398bc66666d
Marcosss87 <marcmansour@outlook.fr>
2023-08-14 14:04:22 +0200

    supress all verification in route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,62 +1,26 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
-import * as z from "zod";
 
 //!Define a schema for input validation
-const userSchema = z.object({
-  name: z.string().min(1, "Username is required").max(50),
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .email("Please enter a valid email"),
-  password: z
-    .string()
-    .min(1, "Password is required")
-    .min(8, "Password must be at least 8 characters long"),
-});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, name, password } = userSchema.parse(body);
-
-    //!Check if email already exists
-    const existingUserByEmail = await prisma.user.findUnique({
-      where: { email: email },
-    });
-    if (existingUserByEmail) {
-      return NextResponse.json(
-        { user: null, message: "User with this email already exists" },
-        { status: 409 }
-      );
-    }
-    //!Check if username already exists
-    const existingUserByUsername = await prisma.user.findUnique({
-      where: { name: name },
-    });
-    if (existingUserByUsername) {
-      return NextResponse.json(
-        { user: null, message: "User with this name already exists" },
-        { status: 409 }
-      );
-    }
+    const { email, name, password } = body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await prisma.user.create({
       data: {
         email,
         name,
-        password: hashedPassword,
+        hashedPassword,
       },
     });
-    const { password: newUserPassword, ...rest } = newUser;
 
-    return NextResponse.json(
-      { user: rest, message: "User created successfully" },
-      { status: 201 }
-    );
+    return NextResponse.json(newUser);
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { message: "Something went wrong" },
       { status: 500 }
