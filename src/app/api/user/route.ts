ada18ee81c452834a06586993dc58cbfb593b0cf
Marcosss87 <marcmansour@outlook.fr>
2023-08-14 11:55:28 +0200

    removing z in route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,25 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import * as z from "zod";
 import bcrypt from "bcrypt";
-
-//!Define a schema for input validation
-const userSchema = z.object({
-  name: z.string().min(1, "Username is required").max(50),
-  email: z
-    .string()
-    .min(1, "Email is required")
-    .email("Please enter a valid email"),
-  password: z
-    .string()
-    .min(1, "Password is required")
-    .min(8, "Password must be at least 8 characters long"),
-});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, name, password } = userSchema.parse(body);
+    const { email, name, password } = body;
 
     //!Check if email already exists
     const existingUserByEmail = await prisma.user.findUnique({
